2bceac135454dfc759801c3802f684091f5db5d4
Daniel Tolan <danieltolan@gmail.com>
2025-07-12 23:41:49 -1000

    build script

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,11 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
+  // GitHub Pages expects an index.html in the root directory
+  // so just run npm build before pushing to GitHub and this will rebuild our assets to the root
+  build: { outDir: '..' },
+  // needed for github pages just put the repo name here
+  base: '/barcelona/',
   plugins: [
     tailwindcss(),
     react(),
