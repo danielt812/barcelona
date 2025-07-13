54c005f800bd72917da47e873a9708a6e3e811b0
Daniel Tolan <danieltolan@gmail.com>
2025-07-13 00:51:16 -1000

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,11 +3,9 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  const isProd = mode === "production";
-
+export default defineConfig(() => {
   return {
-    base: isProd ? "/barcelona/" : "/",
+    base: "/barcelona/",
     build: {
       outDir: "dist",
       emptyOutDir: true,
